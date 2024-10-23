6999971612cf16c0d70e4fac6eece43d73d83584
Joshua Gray <graydog.8719@gmail.com>
2024-10-23 12:35:44 -0600

    set up react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import PokemonPage from "./pages/PokemonPage.jsx";
+import TeamsPage from "./pages/TeamsPage.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+      <Route index element={<HomePage />} />
+      <Route path="pokemon/:id" element={<PokemonPage />} />
+      <Route path="teams" element={<TeamsPage />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
