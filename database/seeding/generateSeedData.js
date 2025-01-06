6be8b4a5a67b481cbd90eb49ceb8a8194221b22c
Joshua Gray <graydog.8719@gmail.com>
2025-01-06 01:10:01 -0700

    reworked seedPokemon.js to only seed pokemon and movesets are now seeded in their own files

--- database/seeding/generateSeedData.js
+++ database/seeding/generateSeedData.js
@@ -54,7 +54,7 @@ const generateJSONs = async (filePath, list, concatenator) => {
 await generateJSONs(
   "./database/seeding/table_JSONs/pokemon/all_pokemon_",
   pokemonList,
-  50
+  200
 );
 
 // generate pokemon species jsons
